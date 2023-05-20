997497c5fead228ec04525df6c9136e9d0b9023f
Mateus Brandão <mateusfleria@gmail.com>
2023-05-19 22:52:24 -0300

    Componentizando aplicação e criando redirecionamento para login com o github

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Copyright = () => (
+  <div className="text-sm leading-relaxed text-gray-200">
+    Feito com ❤ no NLW da{' '}
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://rocketseat.com.br"
+      className="underline hover:text-gray-100"
+    >
+      Rocketseat
+    </a>
+  </div>
+)
