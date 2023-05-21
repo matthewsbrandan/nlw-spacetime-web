4f4bcbd8037dd3aeeefca40b71de777d4a66d5eb
Mateus Brandão <mateusfleria@gmail.com>
2023-05-20 21:19:10 -0300

    Adicionando middleware e protegendo rotas autenticadas

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -9,7 +9,7 @@ export default function NewMemory(){
         voltar à timeline
       </Link>
 
-      <form action="" className="flex flex-1 flex-col gap-2">
+      <form className="flex flex-1 flex-col gap-2">
         <div className="flex items-center gap-4">
           <label
             htmlFor="midia"
